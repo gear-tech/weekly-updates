9a6550919227e87e6e8de12fbb32d580fb4daad5
clearloop <26088946+clearloop@users.noreply.github.com>
2023-06-17 13:53:37 +0400

    ci(gh-pages): publish the built site to gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+  assetPrefix: isProd ? '/weekly-updates/' : '',
+  images: {
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
